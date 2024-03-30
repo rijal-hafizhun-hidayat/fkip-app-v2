a0e8967b99e79510eb94a3f782897534e26f8b9a
unknown <rijal.1344@gmail.com>
2024-03-30 14:21:01 +0700

    wip: add view login

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoginComponent from './LoginComponent.vue'
+import GuestLayout from '../../layouts/GuestLayout.vue'
+</script>
+<template>
+  <GuestLayout>
+    <LoginComponent />
+  </GuestLayout>
+</template>
